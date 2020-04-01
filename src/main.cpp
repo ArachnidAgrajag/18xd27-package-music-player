ea59853758d19004177040dcc06bb8b14f0527a7
aa@eddie <saikrishna31120@gmail.com>
2020-04-01 13:44:54 +0530

    works in terminal not in vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include "playMusic.hpp"
+#include <cstring>
+#include <string>
+
 int main(){
-        std::cout<<"Hello World!!"<<std::endl;
-        playFile("/home/saikrishna/Downloads/Chumma Kizhi-Masstamilan.in.mp3");
+        std::cout<<"Hello!! Please enter the location of the song. including the file extionsion(mp3 only)"<<std::endl;
+        std::string inputFileLoc;
+        std::getline(std::cin, inputFileLoc);
+        const char * cstr = inputFileLoc.c_str();
+        playFile(cstr);
         return 0;
 }
